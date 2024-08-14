762fcbbecc9946191d8ebf5ca10ffd14a76b2809
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-08-14 13:12:42 -0300

    refactoring using Tasks

---
+++ src/CashFlow.Infra/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,11 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infra.DataAccess;
+
+internal class CashFlowDbContext : DbContext
+{
+    public CashFlowDbContext(DbContextOptions options) : base(options) { }
+    public DbSet<Expense> Expenses { get; set; }
+
+}
